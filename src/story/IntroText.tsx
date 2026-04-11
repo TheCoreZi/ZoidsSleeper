36b34521c94bdd730e70f838d040a2c4bfa1fd0b
JEMoralesD <jorgem@spotify.com>
2026-04-10 18:29:08 -0600

    improved intro speed and dialogs

--- src/story/IntroText.tsx
+++ src/story/IntroText.tsx
@@ -1,4 +1,4 @@
-import { createSignal, For, type Component } from 'solid-js';
+import { For, type Component } from 'solid-js';
 import { t } from '../i18n';
 import { PLANET_INTRO_KEYS } from './introScript';
 
@@ -6,26 +6,12 @@ interface IntroTextProps {
   onComplete: () => void;
 }
 
-const IntroText: Component<IntroTextProps> = (props) => {
-  const [visibleCount, setVisibleCount] = createSignal(1);
-
-  const handleClick = () => {
-    if (visibleCount() < PLANET_INTRO_KEYS.length) {
-      setVisibleCount((c) => c + 1);
-    } else {
-      props.onComplete();
-    }
-  };
-
-  return (
-    <div class="intro-text" onClick={handleClick}>
-      <img class="intro-image" src="images/towns/Zi.jpg" alt="Planet Zi" />
-      <For each={PLANET_INTRO_KEYS.slice(0, visibleCount())}>
-        {(key) => <p>{t(key)}</p>}
-      </For>
-      <div class="intro-continue">{t('ui:click_to_continue')}</div>
-    </div>
-  );
-};
+const IntroText: Component<IntroTextProps> = (props) => (
+  <div class="intro-text" onClick={() => props.onComplete()}>
+    <img class="intro-image" src="images/towns/Zi.jpg" alt="Planet Zi" />
+    <For each={PLANET_INTRO_KEYS}>{(key) => <p>{t(key)}</p>}</For>
+    <div class="intro-continue">{t('ui:click_to_continue')}</div>
+  </div>
+);
 
 export default IntroText;
